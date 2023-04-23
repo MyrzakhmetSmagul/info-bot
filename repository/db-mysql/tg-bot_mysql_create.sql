c7d7cac66f4936b52672407908eff1fa9d4ac053
Myrzakhmet <smagul.myrzakhmet@mail.ru>
2023-04-23 14:41:57 +0600

    Changed the tables in the database

--- repository/db-mysql/tg-bot_mysql_create.sql
+++ repository/db-mysql/tg-bot_mysql_create.sql
@@ -1,14 +1,33 @@
 CREATE TABLE IF NOT EXISTS `message`
 (
     `id`              INT          NOT NULL AUTO_INCREMENT,
-    `message_trigger` VARCHAR(255) NOT NULL UNIQUE,
-    `kz_text`         TEXT         NOT NULL,
-    `ru_text`         TEXT         NOT NULL,
-    `en_text`         TEXT         NOT NULL,
-    `state`           INT DEFAULT 0,
+    `message_trigger` VARCHAR(255) NULL UNIQUE,
+    `text`            TEXT         NOT NULL,
+    `lang`            ENUM ("kz","ru","en") DEFAULT "kz",
     PRIMARY KEY (`id`)
 );
 
+CREATE TABLE IF NOT EXISTS `message_group`
+(
+    `id`         INT NOT NULL AUTO_INCREMENT,
+    `message_id` INT NOT NULL,
+    `state_id`   INT NOT NULL,
+    PRIMARY KEY (`id`),
+    CONSTRAINT `message group_fk0` FOREIGN KEY (`message_id`) REFERENCES `message` (`id`)
+);
+
+CREATE TABLE IF NOT EXISTS `state`
+(
+    `id`   INT  NOT NULL,
+    `name` TEXT NOT NULL,
+    PRIMARY KEY (`id`)
+);
+
+ALTER TABLE `message_group`
+    ADD CONSTRAINT `message_group_fk0` FOREIGN KEY (`message_id`) REFERENCES `message` (`id`);
+
+ALTER TABLE `message_group`
+    ADD CONSTRAINT `message_group_fk1` FOREIGN KEY (`state_id`) REFERENCES `state` (`id`);
 
 CREATE TABLE IF NOT EXISTS `keyboard`
 (
@@ -21,11 +40,11 @@ CREATE TABLE IF NOT EXISTS `keyboard`
 
 CREATE TABLE IF NOT EXISTS `reply_markup`
 (
-    `id`          INT NOT NULL AUTO_INCREMENT,
-    `message_id`  INT NOT NULL,
-    `keyboard_id` INT NOT NULL,
+    `id`               INT NOT NULL AUTO_INCREMENT,
+    `message_group_id` INT NOT NULL,
+    `keyboard_id`      INT NOT NULL,
     PRIMARY KEY (`id`),
-    CONSTRAINT `reply_markup_fk0` FOREIGN KEY (`message_id`) REFERENCES `message` (`id`),
+    CONSTRAINT `reply_markup_fk0` FOREIGN KEY (`message_group_id`) REFERENCES `message_group` (`id`),
     CONSTRAINT `reply_markup_fk1` FOREIGN KEY (`keyboard_id`) REFERENCES `keyboard` (`id`)
 );
 
@@ -40,21 +59,22 @@ CREATE TABLE IF NOT EXISTS `command`
 
 CREATE TABLE IF NOT EXISTS `file`
 (
-    `id`         INT                            NOT NULL AUTO_INCREMENT,
-    `message_id` INT                            NOT NULL,
-    `file_name`  ENUM ("photo", "doc", "video") NOT NULL,
-    `file_type`  TEXT                           NOT NULL,
+    `id`               INT                            NOT NULL AUTO_INCREMENT,
+    `message_group_id` INT                            NOT NULL,
+    `file_name`        ENUM ("photo", "doc", "video") NOT NULL,
+    `file_type`        TEXT                           NOT NULL,
     PRIMARY KEY (`id`),
-    CONSTRAINT `file_fk0` FOREIGN KEY (`message_id`) REFERENCES `message` (`id`)
+    CONSTRAINT `file_fk0` FOREIGN KEY (`message_group_id`) REFERENCES `message_group` (`id`)
 );
 
 
 CREATE TABLE IF NOT EXISTS `chat`
 (
-    `chat_id` INT     NOT NULL,
-    `active`  BOOLEAN NOT NULL,
-    `lang`    ENUM ("kz","ru","en") DEFAULT "ru",
-    `state`   INT                   DEFAULT 0,
+    `chat_id`    INT     NOT NULL,
+    `active`     BOOLEAN NOT NULL,
+    `lang`       ENUM ("kz","ru","en") DEFAULT "ru",
+    `state`      INT                   DEFAULT 0,
+    `prev_state` INT                   DEFAULT 0,
+    `cmd`        boolean,
     PRIMARY KEY (`chat_id`)
 );
-
